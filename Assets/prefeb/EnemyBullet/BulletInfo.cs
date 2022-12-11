4cb2a106e8bb24396454681418ec1377a386f649
NEKOOAO <60414oscar@gmail.com>
2022-12-12 01:20:20 +0800

    BulletMannger UPD
    BulletMannger and SO experiment

---
+++ Assets/prefeb/EnemyBullet/BulletInfo.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewBullet", menuName = "CreateSO/Create Enemy Bullet")]
+public class BulletInfo : ScriptableObject
+{
+
+}
